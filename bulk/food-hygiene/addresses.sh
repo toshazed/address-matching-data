124da68eb4165758dedad703949d02ce81aff1a6
psd <psd@whatfettle.com>
2016-07-28 20:25:52 +0100

    Remove trailing comma/space
    This fixes #12

--- bulk/food-hygiene/addresses.sh
+++ bulk/food-hygiene/addresses.sh
@@ -3,4 +3,4 @@
 echo "test	name	text	postcode	food-authority	point"
 find ./cache -name FHRS*.xml |
   xargs xsltproc addresses.xsl |
-  sed -e 's/, [, ]*/, /g' -e 's/, *	//' -e 's/\\//g'
+  sed -e 's/, [, ]*/, /g' -e 's/, *	/	/' -e 's/\\//g'
